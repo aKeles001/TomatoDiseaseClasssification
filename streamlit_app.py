20722584514f832740a72cb6d20f93dc915982cd
Ahmet Keleş <72813684+aKeles001@users.noreply.github.com>
2025-07-31 10:25:40 +0000

    Inıtial Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from PIL import Image
+from tensorflow.keras.models import load_model
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Disease Classification")
+
+model = load_model('/model.h5')
+
+image = st.file_uploader(":arrow_up_small:  ", type=["png", "jpg", "jpeg"])
+col1, col2 = st.columns(2)
+
+
+if st.button("Predict"):
+    image1 = Image.open(image)
+    col1.image(image)
